7718536e705c27945a5ff975238f5be1a0c4f26b
student <student@A26-14>
2023-01-03 10:51:41 +0200

    Add project files.

---
+++ IntentClassWork/MainActivity.cs
@@ -0,0 +1,97 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System;
+
+namespace IntentClassWork
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private EditText etWord;
+        private Button btnSave;
+        private Button btnChoose;
+
+        private string[] words;
+        private int index;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            InitializeViews();
+
+            words = new string[3];
+            index = 0;
+        }
+
+        private void InitializeViews()
+        {
+            etWord = FindViewById<EditText>(Resource.Id.etWord);
+            btnChoose = FindViewById<Button>(Resource.Id.btnChoose);
+            btnSave = FindViewById<Button>(Resource.Id.btnSave);
+
+            btnChoose.Click += btnChoose_click;
+            btnSave.Click += btnSave_click;
+        }
+
+        private void btnSave_click(object sender, EventArgs e)
+        {
+            if (etWord.Text == "")
+                Toast.MakeText(this, "Please enter word", ToastLength.Short).Show();
+            else
+            {
+                words[index] = etWord.Text;
+                index++;
+
+                etWord.Text = "";
+                etWord.Hint = "Enter word #" + (index + 1);
+
+                if (index >= words.Length)
+                {
+                    btnSave.Enabled = false;
+                    btnChoose.Enabled = true;
+                }
+            }
+        }
+
+        private void btnChoose_click(object sender, EventArgs e)
+        {
+            string word1;
+            string word2;
+            Random rnd = new Random();
+
+            word1 = words[rnd.Next(0, words.Length - 1)];
+            word2 = words[rnd.Next(0, words.Length - 1)];
+
+            Intent intent = new Intent(this, typeof(MainActivityWords));
+            intent.PutExtra("WORD1", word1);
+            intent.PutExtra("WORD2", word2);
+            StartActivityForResult(intent, 1);
+        }
+        protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+
+            string word = data.GetStringExtra("WORD");
+            //Toast.MakeText(this, word, ToastLength.Short).Show();
+            Android.App.AlertDialog.Builder builder = new Android.App.AlertDialog.Builder(this, 1);
+            builder.SetTitle("The word you chose:");
+            builder.SetMessage(word);
+            builder.SetPositiveButton("OK", (c, ev) => { });
+            builder.Show();
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ IntentClassWork/MainActivityWords.cs
@@ -0,0 +1,69 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IntentClassWork
+{
+    [Activity(Label = "MainActivityWords")]
+    public class MainActivityWords : Activity
+    {
+        private Button btnWord1;
+        private Button btnWord2;
+        private Button btnReturn;
+
+        private string word;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.activity_mainWords);
+
+            InitializeViews();
+
+            btnWord1.Text = Intent.GetStringExtra("WORD1");
+            btnWord2.Text = Intent.GetStringExtra("WORD2");
+        }
+
+        private void InitializeViews()
+        {
+            btnWord1 = FindViewById<Button>(Resource.Id.btnWord1);
+            btnWord2 = FindViewById<Button>(Resource.Id.btnWord2);
+            btnReturn = FindViewById<Button>(Resource.Id.btnReturn);
+
+            btnWord1.Click += btnWord1_click;
+            btnWord2.Click += btnWord2_click;
+            btnReturn.Click += btnReturn_click;
+        }
+
+        private void btnReturn_click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(word))
+                Toast.MakeText(this, "Please choose a word", ToastLength.Short).Show();
+            else
+            {
+                Intent intent = new Intent();
+                intent.PutExtra("WORD", word);
+                SetResult(Result.Canceled, intent);
+                Finish();
+            }
+        }
+
+        private void btnWord2_click(object sender, EventArgs e)
+        {
+            word = btnWord2.Text;
+        }
+
+        private void btnWord1_click(object sender, EventArgs e)
+        {
+            word = (sender as Button).Text;
+        }
+    }
+}
